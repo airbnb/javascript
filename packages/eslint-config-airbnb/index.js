9e61898f730e3abd6470da3a390988fe24fabd42
Jake Teton-Landis <jake.tl@airbnb.com>
2015-05-22 11:06:16 -0700

    manually load commented JSON

--- packages/eslint-config-airbnb/index.js
+++ packages/eslint-config-airbnb/index.js
@@ -1,1 +1,11 @@
-module.exports = require('airbnb-style/linters/.eslintrc');
+var resolve = require('resolve');
+var stripComments = require('strip-json-comments');
+var fs = require('fs');
+
+// you could do this all at once if you wanted to look cool
+var filename = resolve.sync('airbnb-style/linters/.eslintrc');
+var data = fs.readFileSync(filename, {encoding: 'utf-8'});
+var dataWithoutComments = stripComments(data);
+var parsed = JSON.parse(dataWithoutComments);
+
+module.exports = parsed;
