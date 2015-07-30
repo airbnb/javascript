c443f528f7d7159d32887a8a0ee012170a7a7380
Jake Teton-Landis <jake.tl@airbnb.com>
2015-07-30 14:47:42 -0700

    [eslint] move .eslintrc into packages/eslint-config-airbnb

--- packages/eslint-config-airbnb/index.js
+++ packages/eslint-config-airbnb/index.js
@@ -1,9 +1,9 @@
-var resolve = require('resolve');
+var path = require('path');
 var stripComments = require('strip-json-comments');
 var fs = require('fs');
 
 // you could do this all at once if you wanted to look cool
-var filename = resolve.sync('airbnb-style/linters/.eslintrc');
+var filename = path.join(__dirname, '.eslintrc');
 var data = fs.readFileSync(filename, {encoding: 'utf-8'});
 var dataWithoutComments = stripComments(data);
 var parsed = JSON.parse(dataWithoutComments);
