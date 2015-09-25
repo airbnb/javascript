daea8a638f1f42cd609f1b68b748ad323be31c10
Jimmy Jia <tesrin@gmail.com>
2015-09-25 16:15:38 -0400

    Use rules from eslint-config-defaults

--- packages/eslint-config-airbnb/index.js
+++ packages/eslint-config-airbnb/index.js
@@ -1,13 +1,7 @@
-const reactRules = require('./react');
-const base = require('./base');
-
-// clone this so we aren't mutating a module
-const eslintrc = JSON.parse(JSON.stringify(base));
-
-// manually merge in React rules
-eslintrc.plugins = reactRules.plugins;
-Object.keys(reactRules.rules).forEach(function assignRule(ruleId) {
-  eslintrc.rules[ruleId] = reactRules.rules[ruleId];
-});
-
-module.exports = eslintrc;
+module.exports = {
+  'extends': [
+    // Need to qualify these here for ESLint to resolve them properly.
+    'airbnb/base',
+    'airbnb/rules/react.js'
+  ]
+};
---
+++ packages/eslint-config-airbnb/rules/strict.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'rules': {
+    // require that all functions are run in strict mode
+    'strict': [2, 'never']
+  }
+};
