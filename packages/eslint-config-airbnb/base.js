004bb632b1e8f3e65520738ec3baaa99670e4800
Jake Teton-Landis <jake.tl@airbnb.com>
2015-09-27 18:47:13 -0700

    create legacy.js to export es5 rules

--- packages/eslint-config-airbnb/base.js
+++ packages/eslint-config-airbnb/base.js
@@ -1,23 +1,7 @@
 module.exports = {
   'extends': [
-    './rules/best-practices.js',
-    './rules/errors.js',
+    './legacy.js',
     './rules/es6.js',
-    './rules/legacy.js',
-    './rules/node.js',
-    './rules/strict.js',
-    './rules/style.js',
-    './rules/variables.js'
   ],
   'parser': 'babel-eslint',
-  'env': {
-    'browser': true,
-    'node': true,
-    'amd': false,
-    'mocha': false,
-    'jasmine': false
-  },
-  'ecmaFeatures': {},
-  'globals': {},
-  'rules': {}
 };
