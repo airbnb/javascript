2b744a0a5ab25b02ba53acffa00e371fae35c3f4
Ludovico Fischer <livrerie@gmail.com>
2015-10-12 22:01:42 +0200

    Allow '==' to check for null and undefined in one go.

--- packages/eslint-config-airbnb/rules/best-practices.js
+++ packages/eslint-config-airbnb/rules/best-practices.js
@@ -17,7 +17,7 @@ module.exports = {
     // enforces consistent newlines before or after dots
     'dot-location': 0,
     // require the use of === and !==
-    'eqeqeq': 2,
+    'eqeqeq': [2, 'allow-null'],
     // make sure for-in loops have an if statement
     'guard-for-in': 2,
     // disallow the use of alert, confirm, and prompt
