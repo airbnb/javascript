be532db548d88cac326e1bb95da12be6f7080bca
Adam Alboyadjian <adam@vistahigherlearning.com>
2015-10-15 13:37:25 -0400

    vhl-variant: Add angular global, jasmine and jquery modules.

--- packages/eslint-config-airbnb/legacy.js
+++ packages/eslint-config-airbnb/legacy.js
@@ -13,9 +13,12 @@ module.exports = {
     'node': true,
     'amd': false,
     'mocha': false,
-    'jasmine': false
+    'jquery': true,
+    'jasmine': true
   },
   'ecmaFeatures': {},
-  'globals': {},
+  'globals': {
+    'angular': true
+  },
   'rules': {}
 };
