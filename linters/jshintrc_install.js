eb9f0035c77f3c2b1004acb41fc0926cdb326857
Nigel Kibodeaux <nigel@team.about.me>
2015-11-19 12:29:02 -0900

    moved to npm private package

--- linters/jshintrc_install.js
+++ linters/jshintrc_install.js
@@ -4,4 +4,4 @@
 var fs = require('fs');
 
 // copies the jshintrc file to the root of the parent module's dir
-fs.writeFileSync('../../.jshintrc', fs.readFileSync('./linters/jshintrc'));
+fs.writeFileSync('../../../.jshintrc', fs.readFileSync('./linters/jshintrc'));
