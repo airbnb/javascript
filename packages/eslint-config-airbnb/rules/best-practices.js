f7971301b08d8fc870a127167804059ab2b7c71b
Thomas Shafer <thomasjshafer@gmail.com>
2015-12-16 20:21:38 -0800

    update examples for style add link to no-param-reassign rule documentation

--- packages/eslint-config-airbnb/rules/best-practices.js
+++ packages/eslint-config-airbnb/rules/best-practices.js
@@ -75,6 +75,7 @@ module.exports = {
     'no-octal-escape': 2,
     // disallow reassignment of function parameters
     // disallow parameter object manipulation
+    // rule: http://eslint.org/docs/rules/no-param-reassign.html
     'no-param-reassign': [2, { 'props': true }],
     // disallow use of process.env
     'no-process-env': 0,
