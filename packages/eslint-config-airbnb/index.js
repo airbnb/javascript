55a36d84365949cb5a6a271ebb9c7044cada58d2
Aleksey Kislov <evenfrosty@gmail.com>
2015-12-24 11:31:33 +0300

    Preserve strict rule for default export

--- packages/eslint-config-airbnb/index.js
+++ packages/eslint-config-airbnb/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   'extends': [
     'eslint-config-airbnb/base',
+    'eslint-config-airbnb/rules/strict',
     'eslint-config-airbnb/rules/react',
   ].map(require.resolve),
   rules: {}
