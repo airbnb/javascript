cb153613a0b6f73bf13f6cc0b84f0e3a1c9937bd
Nigel Kibodeaux <nigel@team.about.me>
2015-12-30 11:07:01 -0900

    removed unused files

--- linters/jshintrc_install.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-var fs = require('fs');
-
-// copies the jshintrc file to the root of the parent module's dir
-fs.writeFileSync('../../../.jshintrc', fs.readFileSync('./linters/jshintrc'));
