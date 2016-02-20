82c9dabfb88b6c45e81f7636e756e5acda08278d
Sergey Fursov <geyser85@gmail.com>
2016-02-21 01:31:20 +0300

    rearrange comma-dangle rule to match es5/es6 codestyles, fixes #741

--- packages/eslint-config-airbnb/rules/errors.js
+++ packages/eslint-config-airbnb/rules/errors.js
@@ -1,7 +1,5 @@
 module.exports = {
   'rules': {
-    // disallow trailing commas in object literals
-    'comma-dangle': [2, 'always-multiline'],
     // disallow assignment in conditional expressions
     'no-cond-assign': [2, 'always'],
     // disallow use of console
