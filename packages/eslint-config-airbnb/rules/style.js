956f91527b094909dade4c3d245fc01184c39414
Jordan Harband <ljharb@gmail.com>
2016-04-12 15:25:53 -0700

    [eslint config] [semver-minor] enforce `no-underscore-dangle`

--- packages/eslint-config-airbnb/rules/style.js
+++ packages/eslint-config-airbnb/rules/style.js
@@ -88,7 +88,7 @@ module.exports = {
     // disallow trailing whitespace at the end of lines
     'no-trailing-spaces': 2,
     // disallow dangling underscores in identifiers
-    'no-underscore-dangle': 0,
+    'no-underscore-dangle': [2, { 'allowAfterThis': false }],
     // disallow the use of Boolean literals in conditional expressions
     // also, prefer `a || b` over `a ? a : b`
     // http://eslint.org/docs/rules/no-unneeded-ternary
