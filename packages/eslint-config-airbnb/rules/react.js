c97bd772b4b3b8848a946dd016fa50648299661d
Jordan Harband <ljharb@gmail.com>
2016-04-17 13:22:41 -0700

    [eslint config] [react] separate a11y rules to their own file

--- packages/eslint-config-airbnb/rules/react.js
+++ packages/eslint-config-airbnb/rules/react.js
@@ -1,6 +1,5 @@
 module.exports = {
   'plugins': [
-    'jsx-a11y',
     'react'
   ],
   'ecmaFeatures': {
@@ -9,22 +8,6 @@ module.exports = {
   // View link below for react rules documentation
   // https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules
   'rules': {
-    // Prevent use of `accessKey`
-    // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-access-key.md
-    'jsx-a11y/no-access-key': 2,
-
-    // Require <img> to have a non-empty `alt` prop, or role="presentation"
-    // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/img-uses-alt.md
-    'jsx-a11y/img-uses-alt': 2,
-
-    // Prevent img alt text from containing redundant words like "image", "picture", or "photo"
-    // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/redundant-alt.md
-    'jsx-a11y/redundant-alt': 2,
-
-    // Require ARIA roles to be valid and non-abstract
-    // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/valid-aria-role.md
-    'jsx-a11y/valid-aria-role': 2,
-
     // Prevent missing displayName in a React component definition
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
     'react/display-name': [0, { 'ignoreTranspilerName': false }],
