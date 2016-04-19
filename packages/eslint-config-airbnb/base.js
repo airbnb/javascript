d8f98d8bbafebcf6cddff08efa20370ec6acf68a
Hofmann, Janko <Janko.Hofmann@apertomove.de>
2016-04-19 16:51:39 +0200

    removed unnecessary files

--- packages/eslint-config-airbnb/base.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: [
-    'eslint-config-airbnb/legacy',
-    'eslint-config-airbnb/rules/es6',
-  ].map(require.resolve),
-  rules: {}
-};
--- packages/eslint-config-airbnb/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    'eslint-config-airbnb/base',
-    'eslint-config-airbnb/rules/strict',
-    'eslint-config-airbnb/rules/react',
-  ].map(require.resolve),
-  rules: {}
-};
