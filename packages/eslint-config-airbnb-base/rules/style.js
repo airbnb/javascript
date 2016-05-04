4dc7fa33514feefc29506a11984e2b9a398ac152
Jordan Harband <ljharb@gmail.com>
2016-05-03 22:15:25 -0700

    [eslint config] [base] [breaking] Re-enabling `newline-per-chained-call`
    Per https://github.com/airbnb/javascript/issues/748#issuecomment-216690840 and https://github.com/eslint/eslint/pull/5365

--- packages/eslint-config-airbnb-base/rules/style.js
+++ packages/eslint-config-airbnb-base/rules/style.js
@@ -70,7 +70,7 @@ module.exports = {
     // enforces new line after each method call in the chain to make it
     // more readable and easy to maintain
     // http://eslint.org/docs/rules/newline-per-chained-call
-    'newline-per-chained-call': [0, { 'ignoreChainWithDepth': 3 }],
+    'newline-per-chained-call': [2, { 'ignoreChainWithDepth': 3 }],
     // disallow use of the Array constructor
     'no-array-constructor': 2,
     // disallow use of the continue statement
