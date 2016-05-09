530fa63a7950917d8301b050f4c973dae7e0595d
Jordan Harband <ljharb@gmail.com>
2016-05-09 15:41:52 -0700

    [eslint config] [react] [patch] loosen `jsx-pascal-case` rule to allow all caps component names

--- packages/eslint-config-airbnb/rules/react.js
+++ packages/eslint-config-airbnb/rules/react.js
@@ -64,7 +64,10 @@ module.exports = {
     'react/jsx-no-undef': 2,
     // Enforce PascalCase for user-defined JSX components
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
-    'react/jsx-pascal-case': 2,
+    'react/jsx-pascal-case': [2, {
+      'allowAllCaps': true,
+      'ignore': [],
+    }],
     // Enforce propTypes declarations alphabetical sorting
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-prop-types.md
     'react/sort-prop-types': [0, {
