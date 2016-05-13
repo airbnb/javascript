003ecc1d8c1784f00ca82056031ee258213d99a1
Gregory Waxman <greg@virtru.com>
2016-05-13 11:23:52 -0400

    Updated to default to the more aggressive lint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
 var virtruLintConfig = require('./lib/util').getEnv();
-module.exports = require('./lib/' + (virtruLintConfig.repoEnvironment || process.env.REPO_ENVIRONMENT || 'dev'));
+module.exports = require('./lib/' + (virtruLintConfig.repoEnvironment || process.env.REPO_ENVIRONMENT || 'ci'));
