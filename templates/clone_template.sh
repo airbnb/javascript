8aa3195e01b3d83a2d613334f15e6fdc3efd9129
Matt Casey <matt@x.ai>
2016-06-14 01:38:21 -0400

    create script to create template in utils

--- templates/clone_template.sh
+++ templates/clone_template.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
+### usage: ./clone_template.sh qa my_qa_script
+
+PROJECT_NAME=$2
+
+if [ -z "$1" ] ; then
+    echo 'Sorry! Please define a template'
+    exit 1
+fi
+
+if [ -z "$2" ] ; then
+    echo 'Sorry! Please define a project name'
+    exit 1
+fi
+
 case "$1" in
 
 "qa")
-	echo "creating data qa script in utils"
-	SOURCE=./data_qa
-	DESTINATION=~/xdotai/utils/db/
+	if [ $# -eq 1 ]; then
+		echo "No arguments supplied"
+	fi
+	SOURCE="./data_qa/*"
+	DESTINATION="${HOME}/xdotai/utils/db/data_qa/${PROJECT_NAME}"
 	;;
+
 *) echo "Template not defined for $1"
 	;;
+
 esac
 
-cp -r $SOURCE $DESTINATION
+if [ -n "${SOURCE}" ]; then
+	echo "Copying files from ${SOURCE} to ${DESTINATION}..."
+	mkdir $DESTINATION
+	cp -r $SOURCE $DESTINATION
+	echo "Success!"
+fi
