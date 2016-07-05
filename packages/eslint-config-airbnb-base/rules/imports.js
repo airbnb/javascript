9e47b5092fc835f41d6d8507cf7fc0602121df0c
Joe Lencioni <joe.lencioni@gmail.com>
2016-07-05 10:04:28 -0700

    Add import/no-restricted-paths to imports rules
    This rule was added by 1.10.0. I am setting it to be disabled because it
    is very project-specific, so it doesn't make sense to enable it here.
    
    https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-restricted-paths.md
    
    I found the sections of this file a little confusing, so I didn't know
    where to put it. I think it would be better to simply alphabetize the
    rules but I'll leave that for another time.

--- packages/eslint-config-airbnb-base/rules/imports.js
+++ packages/eslint-config-airbnb-base/rules/imports.js
@@ -124,5 +124,9 @@ module.exports = {
     // Require modules with a single export to use a default export
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
     'import/prefer-default-export': 2,
+
+    // Restrict which files can be imported in a given folder
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-restricted-paths.md
+    'import/no-restricted-paths': 0,
   },
 };
