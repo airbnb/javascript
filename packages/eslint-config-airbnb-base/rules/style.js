99287c6a7c7b97617b23b63e48dca76eaf749b08
Jordan Harband <ljharb@gmail.com>
2016-09-10 14:29:35 -0700

    [eslint config] [base] [patch] loosen `max-len` by ignoring strings
    Per https://github.com/eslint/eslint/pull/7049 / https://github.com/eslint/eslint/issues/5805

--- packages/eslint-config-airbnb-base/rules/style.js
+++ packages/eslint-config-airbnb-base/rules/style.js
@@ -104,7 +104,9 @@ module.exports = {
     // http://eslint.org/docs/rules/max-len
     'max-len': ['error', 100, 2, {
       ignoreUrls: true,
-      ignoreComments: false
+      ignoreComments: false,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
     }],
 
     // specify the max number of lines in a file
