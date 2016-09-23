2b7703cbf03bada33c76901038f720e951e58156
Jordan Harband <ljharb@gmail.com>
2016-09-23 15:29:36 -0700

    [eslint config] [*] [deps] update `eslint-plugin-import`, `eslint` to 3.6

--- packages/eslint-config-airbnb-base/rules/style.js
+++ packages/eslint-config-airbnb-base/rules/style.js
@@ -26,7 +26,7 @@ module.exports = {
     'consistent-this': 'off',
 
     // enforce newline at the end of file, with no multiple empty lines
-    'eol-last': 'error',
+    'eol-last': ['error', 'always'],
 
     // enforce spacing between functions and their invocations
     // http://eslint.org/docs/rules/func-call-spacing
