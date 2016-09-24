0faabe054d0026806e12386f81c508f3ea3386fe
Jordan Harband <ljharb@gmail.com>
2016-09-24 14:30:57 -0700

    [eslint config] [breaking] [deps] update `eslint-config-airbnb-base`

--- packages/eslint-config-airbnb/test/test-base.js
+++ packages/eslint-config-airbnb/test/test-base.js
@@ -11,6 +11,7 @@ fs.readdirSync(path.join(__dirname, '../rules')).forEach((name) => {
     return;
   }
 
+  // eslint-disable-next-line import/no-dynamic-require
   files[name] = require(`../rules/${name}`); // eslint-disable-line global-require
 });
 
