4b6a1ed62919bfd27073b5204e8bfe318278e529
Jordan Harband <ljharb@gmail.com>
2016-11-07 00:48:12 -0800

    [eslint config] [base] [fix] legacy config should not require `**`
    Per https://github.com/airbnb/javascript/commit/1399b3cbfa785f760c15ec63112f711b2c68fd28#commitcomment-19718532

--- packages/eslint-config-airbnb-base/legacy.js
+++ packages/eslint-config-airbnb-base/legacy.js
@@ -18,5 +18,16 @@ module.exports = {
   rules: {
     'comma-dangle': ['error', 'never'],
     'prefer-numeric-literals': 'off',
+    'no-restricted-properties': ['error', {
+      object: 'arguments',
+      property: 'callee',
+      message: 'arguments.callee is deprecated',
+    }, {
+      property: '__defineGetter__',
+      message: 'Please use Object.defineProperty instead.',
+    }, {
+      property: '__defineSetter__',
+      message: 'Please use Object.defineProperty instead.',
+    }],
   }
 };
