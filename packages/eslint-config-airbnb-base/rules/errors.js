e037d29b93e5b61434deb9ba485f76c6406b7cb3
Jordan Harband <ljharb@gmail.com>
2016-12-11 10:44:37 -0800

    [eslint config] [base] [deps] [breaking] update `eslint`; enable `no-await-in-loop`

--- packages/eslint-config-airbnb-base/rules/errors.js
+++ packages/eslint-config-airbnb-base/rules/errors.js
@@ -9,6 +9,10 @@ module.exports = {
       functions: 'always-multiline',
     }],
 
+    // Disallow await inside of loops
+    // http://eslint.org/docs/rules/no-await-in-loop
+    'no-await-in-loop': 'error',
+
     // disallow assignment in conditional expressions
     'no-cond-assign': ['error', 'always'],
 
