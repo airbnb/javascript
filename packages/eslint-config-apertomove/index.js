92c656c6b26d06ecfecb84879e5cc33ed27b1312
Hofmann, Janko <Janko.Hofmann@apertomove.de>
2016-12-19 18:04:16 +0100

    #updated npm package to v2.1.0

--- packages/eslint-config-apertomove/index.js
+++ packages/eslint-config-apertomove/index.js
@@ -1,21 +1,53 @@
 module.exports = {
-    extends: "airbnb/base",
+    extends: "airbnb",
     parserOptions: {
         "ecmaVersion": 6,
         "sourceType": "module"
     },
     rules: {
-        "indent": [2, 4], // we prefer 4 spaces
-        "comma-dangle": [2, "never"], // we prefer no trailing commas
-        "no-labels": [2, {"allowLoop": false, "allowSwitch": false}],
-        "keyword-spacing": [2, {"before": true, "after": true, "overrides": {}}],
-        "max-len": 0, // don't limit line length
-        "no-use-before-define": 0, // while this makes sense, it also errors when used in functions that are not called yet
-        "no-unused-vars": [1, {"vars": "all", "args": "after-used"}], // we only warn about unused vars
-        "no-empty-label": 0, // deprecated rules specified in airbnb preset
-        "space-before-keywords": 0,
-        "space-after-keywords": 0,
-        "space-return-throw-case": 0,
-        "arrow-body-style": [2, "always"] // always surround the execution block in arrow functions with curly braces
+        "indent": [
+            "error",
+            4, {
+                "SwitchCase": 1,
+                "VariableDeclarator": 1,
+                "outerIIFEBody": 1
+            }
+        ], // we prefer 4 spaces
+        "react/jsx-indent": [
+            "warn", 4
+        ],
+        "react/jsx-indent-props": [
+            "warn", 4
+        ],
+        "comma-dangle": [
+            "error", "never"
+        ], // we prefer no trailing commas in objects
+        "no-unused-vars": [
+            "warn", {
+                vars: "local",
+                args: "after-used"
+            }
+        ], // we only warn about unused vars
+        "max-len": "off", // don't limit line length
+        "class-methods-use-this": "off", // we don't enforce all class methods to use "this", since this makes for an inconsistent API (especially for React components)
+        "no-use-before-define": "off", // while this makes sense, it also errors when used in functions that are not called yet
+        "no-restricted-syntax": [ // we allow 'for...in'-loops
+            "error",
+            "DebuggerStatement",
+            "LabeledStatement",
+            "WithStatement"
+        ],
+        "arrow-parens": [
+            "error",
+            "as-needed", {
+                "requireForBlockBody": false // we don't require parentheses for single function params when block body is used
+            }
+        ],
+        "react/forbid-prop-types": [
+            "warn", { // we only warn on discouraged prop types
+                "forbid": ["any", "array", "object"]
+            }
+        ],
+        "react/no-danger": "off" // sometimes you just can't avoid 'dangerouslySetInnerHTML', for ex. when you receive HTML strings over an API
     }
 };
