1f5999b2ed832bdf341b15a7a0a564e6c2684a0d
Jordan Harband <ljharb@gmail.com>
2017-03-03 13:33:24 -0800

    [eslint config] [base] [deps] update `eslint`
     - fill out options in `no-use-before-define`
     - enable `ignoreRestSiblings` in `no-unused-vars`

--- packages/eslint-config-airbnb-base/rules/variables.js
+++ packages/eslint-config-airbnb-base/rules/variables.js
@@ -34,9 +34,9 @@ module.exports = {
     'no-undefined': 'off',
 
     // disallow declaration of variables that are not used in the code
-    'no-unused-vars': ['error', { vars: 'local', args: 'after-used' }],
+    'no-unused-vars': ['error', { vars: 'local', args: 'after-used', ignoreRestSiblings: true }],
 
     // disallow use of variables before they are defined
-    'no-use-before-define': 'error'
+    'no-use-before-define': ['error', { functions: true, classes: true, variables: true }],
   }
 };
