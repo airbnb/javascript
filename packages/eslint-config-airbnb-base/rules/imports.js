3d033d531d4a5ae0ebae0a84eb3e413760d1c133
Nicolas Jakob <nicow.jakob@gmail.com>
2017-04-24 16:24:54 +0200

    Add rollup.config.*.js to import/no-extraneous-dependencies ignore list

--- packages/eslint-config-airbnb-base/rules/imports.js
+++ packages/eslint-config-airbnb-base/rules/imports.js
@@ -80,6 +80,7 @@ module.exports = {
         '**/webpack.config.js', // webpack config
         '**/webpack.config.*.js', // webpack config
         '**/rollup.config.js', // rollup config
+        '**/rollup.config.*.js', // rollup config
         '**/gulpfile.js', // gulp config
         '**/gulpfile.*.js', // gulp config
         '**/Gruntfile', // grunt config
