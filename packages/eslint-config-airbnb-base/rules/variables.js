f90812b9355937212dc1a4bce27e71c89a0c0872
Siddharth Doshi <sidoshi510@gmail.com>
2017-05-12 01:08:33 +0530

    [eslint config] [base] [minor] Disallow unused global variables

--- packages/eslint-config-airbnb-base/rules/variables.js
+++ packages/eslint-config-airbnb-base/rules/variables.js
@@ -34,7 +34,7 @@ module.exports = {
     'no-undefined': 'off',
 
     // disallow declaration of variables that are not used in the code
-    'no-unused-vars': ['error', { vars: 'local', args: 'after-used', ignoreRestSiblings: true }],
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
 
     // disallow use of variables before they are defined
     'no-use-before-define': ['error', { functions: true, classes: true, variables: true }],
