f86aacae447725d6d0edd787d620fad54ac1b187
Jordan Harband <ljharb@gmail.com>
2017-05-23 12:53:26 -0700

    [eslint config] [fix] jsx should be enabled via parserOptions, not via a root ecmaFeatures.
    From https://github.com/airbnb/javascript/issues/1410#issuecomment-303368921

--- packages/eslint-config-airbnb/rules/react-a11y.js
+++ packages/eslint-config-airbnb/rules/react-a11y.js
@@ -3,9 +3,13 @@ module.exports = {
     'jsx-a11y',
     'react'
   ],
-  ecmaFeatures: {
-    jsx: true
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+
   rules: {
     // Enforce that anchors have content
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-has-content.md
