7cc0625e9f21c9eb4f906b77017df3cf8ab2172d
Jordan Harband <ljharb@gmail.com>
2017-07-04 14:30:29 -0700

    [eslint config] [*] Add missing rules

--- packages/eslint-config-airbnb-base/rules/imports.js
+++ packages/eslint-config-airbnb-base/rules/imports.js
@@ -191,5 +191,16 @@ module.exports = {
     // Prevent importing the default as if it were named
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-default.md
     'import/no-named-default': 'error',
+
+    // Reports if a module's default export is unnamed
+    // https://github.com/benmosher/eslint-plugin-import/blob/d9b712ac7fd1fddc391f7b234827925c160d956f/docs/rules/no-anonymous-default-export.md
+    'import/no-anonymous-default-export': ['off', {
+      allowArray: false,
+      allowArrowFunction: false,
+      allowAnonymousClass: false,
+      allowAnonymousFunction: false,
+      allowLiteral: false,
+      allowObject: false,
+    }],
   },
 };
