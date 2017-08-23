22672c1cdcae9b717ca2710b42f6572f45dea8b5
Jordan Harband <ljharb@gmail.com>
2017-08-22 19:44:12 -0700

    [eslint config] [new] add `propWrapperFunctions` default settings for `eslint-plugin-react`

--- packages/eslint-config-airbnb/rules/react.js
+++ packages/eslint-config-airbnb/rules/react.js
@@ -360,5 +360,10 @@ module.exports = {
       pragma: 'React',
       version: '15.0'
     },
+    propWrapperFunctions: [
+      'forbidExtraProps', // https://www.npmjs.com/package/airbnb-prop-types
+      'exact', // https://www.npmjs.com/package/prop-types-exact
+      'Object.freeze', // https://tc39.github.io/ecma262/#sec-object.freeze
+    ],
   }
 };
