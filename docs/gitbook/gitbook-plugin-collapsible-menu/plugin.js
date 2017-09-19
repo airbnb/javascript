613fa7491016f66ba5941853c942273931d5ace2
Jason Law <jaylaw81@gmail.com>
2017-09-19 13:52:46 -0400

    added jekyll

--- docs/gitbook/gitbook-plugin-collapsible-menu/plugin.js
+++
@@ -1,8 +0,0 @@
-require(["gitbook"], function(gitbook) {
-    gitbook.events.bind("page.change", function() {
-        $('ul.summary li li').hide();
-        // $('ul.summary li').find('li.active').parent().children().show();
-        $('ul.summary li li.active').parents().children(':not(script)').show();
-        $('ul.summary li.active > ul > li').show();        
-    });
-});
