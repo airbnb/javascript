6fbc47256ef0671be7f11cb3016489f6a9afa16a
Sharmila <sharmila.jesupaul@airbnb.com>
2017-10-18 13:34:15 -0700

    Fixes test syles

--- packages/eslint-config-airbnb-base/test/test-base.js
+++ packages/eslint-config-airbnb-base/test/test-base.js
@@ -20,8 +20,8 @@ Object.keys(files).forEach(( // eslint-disable-line function-paren-newline
     t.plan(2);
 
     // scan plugins for react and fail if it is found
-    const hasReactPlugin = Object.prototype.hasOwnProperty.call(config, 'plugins') &&
-      config.plugins.indexOf('react') !== -1;
+    const hasReactPlugin = Object.prototype.hasOwnProperty.call(config, 'plugins')
+      && config.plugins.indexOf('react') !== -1;
     t.notOk(hasReactPlugin, 'there is no react plugin');
 
     // scan rules for react/ and fail if any exist
