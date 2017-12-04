8247e338a2d689f2e16010b0db225fbce7760f97
Remco Haszing <remcohaszing@gmail.com>
2017-12-04 10:11:35 +0100

    [eslint config] [base] [patch] Set import/extensions to ignorePackages
    This allows to import non-JavaScript files through the main export of a dependency's package.json.
    
    The following would trigger an error before, but is fine with the new configuration:
    
    ```js
    import 'roboto-fontface';
    ```

--- packages/eslint-config-airbnb-base/rules/imports.js
+++ packages/eslint-config-airbnb-base/rules/imports.js
@@ -132,7 +132,7 @@ module.exports = {
 
     // Ensure consistent use of file extension within the import path
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
-    'import/extensions': ['error', 'always', {
+    'import/extensions': ['error', 'ignorePackages', {
       js: 'never',
       mjs: 'never',
       jsx: 'never',
