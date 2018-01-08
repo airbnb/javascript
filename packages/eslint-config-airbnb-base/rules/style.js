7b18d7c2155f52937b1d48a3c6b905943fc1e5b7
Jordan Harband <ljharb@gmail.com>
2018-01-08 00:35:15 -0800

    [eslint config] [*] [deps] update `eslint`

--- packages/eslint-config-airbnb-base/rules/style.js
+++ packages/eslint-config-airbnb-base/rules/style.js
@@ -123,7 +123,8 @@ module.exports = {
       ObjectExpression: 1,
       ImportDeclaration: 1,
       flatTernaryExpressions: false,
-      ignoredNodes: ['JSXElement', 'JSXElement *']
+      ignoredNodes: ['JSXElement', 'JSXElement *'],
+      ignoreComments: false
     }],
 
     // specify whether double or single quotes should be used in JSX attributes
