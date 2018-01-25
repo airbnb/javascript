2d2e0f6e742efbaefc616f065baed79107e6be27
MichelML <michmoreau.l@gmail.com>
2018-01-25 17:48:24 -0500

    use tempfile to lint all js and ts files in a repo

--- tsjs.js
+++ tsjs.js
@@ -1,14 +1,32 @@
 #! /usr/bin/env node
 
+const fs = require('fs');
 const argv = require('yargs').argv;
 const sh = require('shelljs');
 const path = require('path');
 const tslintPath = argv.tslint || path.join(__dirname, 'tslint.json');
-const tsConfigPath = argv.tsconfig || path.join(__dirname, 'tsconfig.lint.json');
+const tempLintFile = '.tsconfig.lint.temp.json';
+let tsConfigPath = argv.tsconfig || 'tsconfig.json';
 
-console.log(`Using these files for linting:\ntslint file: ${tslintPath}\ntsconfig file: ${tsConfigPath}`);
+if (argv.all) {
+    try {
+        fs.writeFileSync(tempLintFile, JSON.stringify(require(path.join(__dirname, 'tsconfig.lint.json'))));
+        tsConfigPath = tempLintFile;
+    } catch (e) {
+        console.log(e);
+        console.log('Something went wrong. See logs above for details.');
+        sh.rm('-rf', tempLintFile);
+    }
+}
+
+console.log(`Using following files for linting:\ntslint file: ${tslintPath}\ntsconfig file: ${tsConfigPath}`);
 
 sh.exec(`node node_modules/tslint/bin/tslint --project ${tsConfigPath} --config ${tslintPath} --fix`, () => {
     console.log('\nLint fix completed.');
+
+    if (argv.all) {
+        sh.rm('-rf', tempLintFile);
+    }
+
     process.exit();
 });
