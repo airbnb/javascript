93f9d6a8fb85ce7da097bed052d3a0e368c3b8d9
MichelML <michmoreau.l@gmail.com>
2018-01-24 22:03:51 -0500

    add full path

--- scripts/lintinstall/index.js
+++ scripts/lintinstall/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const tslintConfig = require(`${__dirname}/../../tslint.json`);
 
-fs.writeFileSync('tslint.json', JSON.stringify(tslintConfig, null, 2));
+fs.writeFileSync(`${process.cwd()}/tslint.json`, JSON.stringify(tslintConfig, null, 2));
 process.exit();
