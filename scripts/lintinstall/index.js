e0bdb161bb44c91d06cafb9a182d884a5b1daabb
MichelML <michmoreau.l@gmail.com>
2018-01-25 09:47:23 -0500

    adjust script depending on type of install

--- scripts/lintinstall/index.js
+++ scripts/lintinstall/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const tslintConfig = require(`${__dirname}/../../tslint.json`);
 const tsConfigLint = require(`${__dirname}/../../tsconfig.lint.json`);
-const projectRoot = `${__dirname.split('node_modules')[0]}`;
+const projectRoot = /tsjs$/.test(process.cwd()) ? `${process.cwd()}/` : `${__dirname.split('node_modules')[0]}`;
 
 // tslint:disable-next-line
 console.log('Copying lint files to project root...');
@@ -9,7 +9,7 @@ console.log('Copying lint files to project root...');
 [
     {name: 'tslint.json', content: tslintConfig},
     {name: 'tsconfig.lint.json', content: tsConfigLint},
-].forEach((file) => fs.writeFileSync(`${projectRoot}${file.name}`, JSON.stringify(file.content, null, 2)));
+].forEach((file) => fs.writeFileSync(`${projectRoot}/${file.name}`, `${JSON.stringify(file.content, null, 2)}\n`));
 
 // tslint:disable-next-line
 console.log(`Linting files copied at ${projectRoot}`);
