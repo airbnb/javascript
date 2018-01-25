54cdc4243de2adfc1763659ad471e3c49d5903f7
MichelML <michmoreau.l@gmail.com>
2018-01-24 22:33:55 -0500

    implement final version of script for pr

--- scripts/lintinstall/index.js
+++ scripts/lintinstall/index.js
@@ -1,5 +1,17 @@
 const fs = require('fs');
 const tslintConfig = require(`${__dirname}/../../tslint.json`);
+const tsConfigLint = require(`${__dirname}/../../tsconfig.lint.json`);
+const projectRoot = `${__dirname.split('node_modules')[0]}`;
 
-fs.writeFileSync(`${process.cwd()}/tslint.json`, JSON.stringify(tslintConfig, null, 2));
+// tslint:disable-next-line
+console.log('Copying lint files to project root...');
+
+[
+    {name: 'tslint.json', content: tslintConfig},
+    {name: 'tsconfig.lint.json', content: tsConfigLint},
+].forEach((file) => fs.writeFileSync(`${projectRoot}${file.name}`, JSON.stringify(file.content, null, 2)));
+
+// tslint:disable-next-line
+console.log(`Linting files copied at ${projectRoot}`);
+
 process.exit();
