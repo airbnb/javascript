b8073a977c5dae8c972e8b93fd1d53ed677e9e1f
MichelML <michmoreau.l@gmail.com>
2018-01-25 09:53:32 -0500

    use path.join

--- scripts/lintinstall/index.js
+++ scripts/lintinstall/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 const tslintConfig = require(`${__dirname}/../../tslint.json`);
 const tsConfigLint = require(`${__dirname}/../../tsconfig.lint.json`);
-const projectRoot = /tsjs$/.test(process.cwd()) ? `${process.cwd()}/` : `${__dirname.split('node_modules')[0]}`;
+const projectRoot = /tsjs$/.test(process.cwd()) ? process.cwd() : `${__dirname.split('node_modules')[0]}`;
 
 // tslint:disable-next-line
 console.log('Copying lint files to project root...');
@@ -9,7 +10,7 @@ console.log('Copying lint files to project root...');
 [
     {name: 'tslint.json', content: tslintConfig},
     {name: 'tsconfig.lint.json', content: tsConfigLint},
-].forEach((file) => fs.writeFileSync(`${projectRoot}/${file.name}`, `${JSON.stringify(file.content, null, 2)}\n`));
+].forEach((file) => fs.writeFileSync(path.join(projectRoot, file.name), `${JSON.stringify(file.content, null, 2)}\n`));
 
 // tslint:disable-next-line
 console.log(`Linting files copied at ${projectRoot}`);
