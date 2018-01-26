25f6bc42c5ec2c8629af09fbfbbc6049f55de218
MichelML <michmoreau.l@gmail.com>
2018-01-26 13:54:39 -0500

    add exclude option

--- tsjs.js
+++ tsjs.js
@@ -2,10 +2,12 @@
 
 const fs = require('fs');
 const argv = require('yargs').argv;
+argv.array('exclude');
 const sh = require('shelljs');
 const path = require('path');
 const tslintPath = argv.tslint || path.join(__dirname, 'tslint.json');
 const tempLintFile = '.tsconfig.lint.temp.json';
+const excludeOption = argv.exclude ? `--exclude ${argv.exclude.join(' ')}` : '';
 let tsConfigPath = argv.tsconfig || 'tsconfig.json';
 
 if (argv.all) {
@@ -21,7 +23,7 @@ if (argv.all) {
 
 console.log(`Using following files for linting:\ntslint file: ${tslintPath}\ntsconfig file: ${tsConfigPath}`);
 
-sh.exec(`node node_modules/tslint/bin/tslint --project ${tsConfigPath} --config ${tslintPath} --fix`, () => {
+sh.exec(`node node_modules/tslint/bin/tslint --project ${tsConfigPath} --config ${tslintPath} ${excludeOption} --fix`, () => {
     console.log('\nLint fix completed.');
 
     if (argv.all) {
