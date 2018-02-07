8719d6f9aeb51639b6682de6bfbba1d218e3faf2
Emil Cieslar <c.emil@mindojo.com>
2018-02-07 18:41:07 +0100

    Added ignore for mindojo-core local imports so it doesnt show errors

--- packages/eslint-config-mindojo/rules/mindojo.js
+++ packages/eslint-config-mindojo/rules/mindojo.js
@@ -1,10 +1,17 @@
 module.exports = {
   rules: {
+    'import/no-unresolved': [
+      'error',
+      {
+        ignore: ['mindojo-core', 'mindojo-payments', 'ui-kit']
+      }
+    ],
+
     // Sample rules
     // 'class-methods-use-this': ['error', {
     //   exceptMethods: [],
     // }],
 
-    //'array-callback-return': 'error',
+    // 'array-callback-return': 'error',
   }
 };
