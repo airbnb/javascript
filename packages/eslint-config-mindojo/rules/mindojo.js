12c22e9f65ee960bb80c18706d912172a0616eb4
Nik Shakhmatov <nik.shakhmatov@mindojo.com>
2018-03-05 12:32:35 +0700

    CORE-1644: ESlint refactoring

--- packages/eslint-config-mindojo/rules/mindojo.js
+++ packages/eslint-config-mindojo/rules/mindojo.js
@@ -13,5 +13,41 @@ module.exports = {
     // }],
 
     // 'array-callback-return': 'error',
-  }
+
+    'one-var': 0,
+
+    'one-var-declaration-per-line': ['error', 'initializations'],
+
+    'prefer-destructuring': ['off'],
+
+    'consistent-return': 'warn',
+
+    'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+
+    // specify the maximum length of a line in your program
+    // http://eslint.org/docs/rules/max-len
+    'max-len': ['error', 120, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+
+    // disallow dangling underscores in identifiers
+    'no-underscore-dangle': ['error', {
+      allow: [],
+      allowAfterThis: true,
+      allowAfterSuper: false,
+    }],
+
+    'no-param-reassign': ['warn'],
+
+    // require parens in arrow function arguments
+    // http://eslint.org/docs/rules/arrow-parens
+    'arrow-parens': ['error', 'as-needed', {
+      requireForBlockBody: false,
+    }],
+
+  },
 };
