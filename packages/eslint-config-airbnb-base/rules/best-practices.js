73e517e87db9070c55b07ab5ab2d469cabf10e61
Shyam Chen <shyamchen1994@gmail.com>
2018-03-27 09:23:45 +0800

    feat: support vuex state

--- packages/eslint-config-airbnb-base/rules/best-practices.js
+++ packages/eslint-config-airbnb-base/rules/best-practices.js
@@ -181,7 +181,8 @@ module.exports = {
         'res', // for Express responses
         'response', // for Express responses
         '$scope', // for Angular 1 scopes
-      ]
+        'state', // for Vuex state
+      ],
     }],
 
     // disallow usage of __proto__ property
