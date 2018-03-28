8720f5f90bbe4aaf87e03d81b3b4f607f27ff6c6
C. T. Lin <chentsulin@gmail.com>
2018-03-28 13:05:12 +0800

    [eslint config] [base] [patch] avoid `__mocks__` `no-extraneous-dependencies` check

--- packages/eslint-config-airbnb-base/rules/imports.js
+++ packages/eslint-config-airbnb-base/rules/imports.js
@@ -74,6 +74,7 @@ module.exports = {
         'tests/**', // also common npm pattern
         'spec/**', // mocha, rspec-like pattern
         '**/__tests__/**', // jest pattern
+        '**/__mocks__/**', // jest pattern
         'test.{js,jsx}', // repos with a single test file
         'test-*.{js,jsx}', // repos with multiple top-level test files
         '**/*.{test,spec}.{js,jsx}', // tests where the extension denotes that it is a test
