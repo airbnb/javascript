2d9779907f9760472e0318ad11b01f67a9f5dc7e
Thomas Grainger <tagrain@gmail.com>
2018-04-30 09:30:38 +0100

    [eslint config] [base] [minor] only allow one newline at the end

--- packages/eslint-config-airbnb-base/rules/style.js
+++ packages/eslint-config-airbnb-base/rules/style.js
@@ -302,7 +302,7 @@ module.exports = {
     'no-multi-assign': ['error'],
 
     // disallow multiple empty lines and only one newline at the end
-    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
 
     // disallow negated conditions
     // https://eslint.org/docs/rules/no-negated-condition
