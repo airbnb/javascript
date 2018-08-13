9a5267684b59106523cf5419439fd0d53c0a512e
Jordan Harband <ljharb@gmail.com>
2018-08-13 13:35:23 -0700

    [eslint config] [patch] re-enabling `jsx-one-expression-per-line` allowing single children, ignore DOM components on `jsx-no-bind`; re-enable `restructuring-assignment`
    Reverts b6a268f780177e03b573a4f0df95ecc0d2e8783e

--- packages/eslint-config-airbnb/rules/react.js
+++ packages/eslint-config-airbnb/rules/react.js
@@ -93,7 +93,9 @@ module.exports = {
     'react/jsx-no-bind': ['error', {
       ignoreRefs: true,
       allowArrowFunctions: true,
+      allowFunctions: false,
       allowBind: false,
+      ignoreDOMComponents: true,
     }],
 
     // Prevent duplicate props in JSX
@@ -413,13 +415,11 @@ module.exports = {
 
     // One JSX Element Per Line
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/jsx-one-expression-per-line.md
-    // TODO: re-enable when an option for text children is available
-    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-one-expression-per-line': ['error', { allow: 'single-child' }],
 
     // Enforce consistent usage of destructuring assignment of props, state, and context
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/destructuring-assignment.md
-    // TODO: re-enable when component detection is fixed
-    'react/destructuring-assignment': ['off', 'always'],
+    'react/destructuring-assignment': ['error', 'always'],
 
     // Prevent using this.state within a this.setState
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/no-access-state-in-setstate.md
