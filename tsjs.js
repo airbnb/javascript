740c539276f9ad6a57e8d4d2b2495758cd6eb684
MichelML <michmoreau.l@gmail.com>
2018-08-13 17:48:36 -0400

    add lodash to simplify code

--- tsjs.js
+++ tsjs.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const fs = require('fs');
 const yargs = require('yargs');
+const {flow, update} = require('lodash/fp');
 const {Linter, Configuration} = require('tslint');
 const tsfmt = require('typescript-formatter');
 
@@ -26,19 +27,13 @@ process.on('exit', () => {
 
 try {
     fs.writeFileSync(tempTsfmtFile, JSON.stringify(require(tsfmtPath)));
-
-    let tslintJSON = require(tslintPath);
-    const defaultIgnorePattern = tslintJSON['no-unused-variable'][1]['ignore-pattern'];
-    tslintJSON = {
-        ...tslintJSON,
-        'no-unused-variable': [
-            true,
-            {
-                'ignore-pattern': argv.ignorepattern || defaultIgnorePattern,
-            },
-        ],
-    };
-    fs.writeFileSync(tempTsLintFile, JSON.stringify(require(tslintPath)));
+    fs.writeFileSync(
+        tempTsLintFile,
+        flow(
+            update('no-unused-variable[1].ignore-pattern', (val) => argv.ignorepattern || val),
+            JSON.stringify,
+        )(require(tslintPath)),
+    );
 
     if (argv.all) {
         tsConfigLint = require(tsconfigPath);
