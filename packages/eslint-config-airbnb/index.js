ad0c44c6affa87743419038682f00c0b9d0d9989
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-11-20 12:06:34 +0000

    [eslint config] Remove rules/strict from 'extends'
    Since the file is now imported in base instead.
    
    Fixes #1582.

--- packages/eslint-config-airbnb/index.js
+++ packages/eslint-config-airbnb/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
     'eslint-config-airbnb-base',
-    'eslint-config-airbnb-base/rules/strict',
     './rules/react',
     './rules/react-a11y',
   ].map(require.resolve),
