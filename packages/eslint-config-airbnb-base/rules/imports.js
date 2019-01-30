96f95fa35e23f015448445b8d5bb7ac8e03a739a
tclindner <tclindner@users.noreply.github.com>
2019-01-29 21:58:23 -0600

    [eslint config] [base] [patch] `no-extraneous-dependencies`: Add jest.setup.js to devDeps

--- packages/eslint-config-airbnb-base/rules/imports.js
+++ packages/eslint-config-airbnb-base/rules/imports.js
@@ -79,6 +79,7 @@ module.exports = {
         'test-*.{js,jsx}', // repos with multiple top-level test files
         '**/*{.,_}{test,spec}.{js,jsx}', // tests where the extension or filename suffix denotes that it is a test
         '**/jest.config.js', // jest config
+        '**/jest.setup.js', // jest setup
         '**/vue.config.js', // vue-cli config
         '**/webpack.config.js', // webpack config
         '**/webpack.config.*.js', // webpack config
