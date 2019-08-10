6b5922ca2132980609fe217353162c2338b29ae2
Jordan Harband <ljharb@gmail.com>
2019-08-09 22:39:06 -0700

    [eslint config] [breaking] set react version to "detect"
    Fixes #1983.

--- packages/eslint-config-airbnb/rules/react.js
+++ packages/eslint-config-airbnb/rules/react.js
@@ -515,7 +515,7 @@ module.exports = {
     },
     react: {
       pragma: 'React',
-      version: '16.0'
+      version: 'detect',
     },
     propWrapperFunctions: [
       'forbidExtraProps', // https://www.npmjs.com/package/airbnb-prop-types
