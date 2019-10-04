3fb312f18198364bacc07e61ca58ff89f9f0afa2
Alexander Wesolowski <foglerek@gmail.com>
2019-10-04 15:57:05 -0700

    [eslint config] [patch] Add `static-variables` to `sort-comp` rule

--- packages/eslint-config-airbnb/rules/react.js
+++ packages/eslint-config-airbnb/rules/react.js
@@ -240,6 +240,7 @@ module.exports = {
     // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/sort-comp.md
     'react/sort-comp': ['error', {
       order: [
+        'static-variables',
         'static-methods',
         'instance-variables',
         'lifecycle',
