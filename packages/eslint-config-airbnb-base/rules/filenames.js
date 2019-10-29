11a13646630d481b541e1246b6e2177bc86e6b4d
Kristóf Poduszló <kripod@protonmail.com>
2019-10-29 11:27:52 +0100

    [eslint config] add missing filenames rules

--- packages/eslint-config-airbnb-base/rules/filenames.js
+++ packages/eslint-config-airbnb-base/rules/filenames.js
@@ -2,8 +2,16 @@ module.exports = {
   plugins: ['filenames'],
 
   rules: {
+    // allow any filename casing
+    // https://github.com/selaux/eslint-plugin-filenames#consistent-filenames-via-regex-match-regex
+    'filenames/match-regex': 'off',
+
     // match filename against the default export, accepting kebab-cased names
     // https://github.com/selaux/eslint-plugin-filenames#matching-exported-values-match-exported
     'filenames/match-exported': ['error', [null, 'kebab']],
+
+    // allow using index files
+    // https://github.com/selaux/eslint-plugin-filenames#dont-allow-indexjs-files-no-index
+    'filenames/no-index': 'off'
   },
 };
