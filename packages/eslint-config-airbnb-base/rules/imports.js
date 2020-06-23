c5bee75b1b358a3749f1a6d38ee6fad73de28e29
Jordan Harband <ljharb@gmail.com>
2020-06-22 22:55:52 -0700

    [eslint config] [*] [deps] update `eslint-plugin-import`, use valid `import/no-cycle` `maxDepth` option

--- packages/eslint-config-airbnb-base/rules/imports.js
+++ packages/eslint-config-airbnb-base/rules/imports.js
@@ -233,7 +233,7 @@ module.exports = {
 
     // Forbid cyclical dependencies between modules
     // https://github.com/benmosher/eslint-plugin-import/blob/d81f48a2506182738409805f5272eff4d77c9348/docs/rules/no-cycle.md
-    'import/no-cycle': ['error', { maxDepth: Infinity }],
+    'import/no-cycle': ['error', { maxDepth: '∞' }],
 
     // Ensures that there are no useless path segments
     // https://github.com/benmosher/eslint-plugin-import/blob/ebafcbf59ec9f653b2ac2a0156ca3bcba0a7cf57/docs/rules/no-useless-path-segments.md
