5826b2aa24e523296015d38c1478028f156bac1a
inrmanish <inmanish868@gmail.com>
2022-03-06 21:17:43 -0800

    Fixed Classification of an Object Naming Convention

---
+++ class in js.js
@@ -0,0 +1,12 @@
+class SoftwareDeveloper {
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  }
+  
+  var me = new SoftwareDeveloper('Robin', 'Wieruch');
+
+
+  // Every time a JavaScript constructor is called to instantiate a new instance of a class, the name of the class should appear in Pascal Case, because the class has been declared with Pascal Case in the first place.
+  
