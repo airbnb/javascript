de58edcc806dd5b0ebb3c10170fdf5207779e2c8
Olivier Zalmanski <olivier.zalmanski@me.com>
2023-08-24 16:15:30 +0200

    Imports rules: add eslint flat config support

---
+++ packages/eslint-config-airbnb-base/flat.js
@@ -0,0 +1,64 @@
+/**
+ * Changes to support the flat config format:
+ * - The `extends` key has been deprecated.
+ * - The `parserOptions` key has been moved under the new `languageOptions` key.
+ * - The `env` key has been deprecated and moved to `languageOptions.globals`.
+ * - The `plugins` key is no longer a list but an object.
+ */
+
+const {
+  extends: airbnbRules,
+  ...airbnbConfig
+} = require('eslint-config-airbnb-base');
+const importPlugin = require('eslint-plugin-import');
+const globals = require('globals');
+
+const mappedEnvs = {
+  es6: 'es2015',
+};
+const handledPlugins = {
+  import: importPlugin,
+};
+
+function convertIntoEslintFlatConfig(config) {
+  const newConfig = { ...config, languageOptions: {} };
+
+  // Handle the `extends` key
+  if ('env' in newConfig) {
+    newConfig.languageOptions.globals = Object.keys(newConfig.env)
+      .filter((key) => newConfig.env[key] === true)
+      .reduce((acc, key) => {
+        if (key in globals) {
+          return { ...acc, ...globals[key] };
+        }
+        if (key in mappedEnvs && mappedEnvs[key] in globals) {
+          return { ...acc, ...globals[mappedEnvs[key]] };
+        }
+        return acc;
+      }, {});
+    delete newConfig.env;
+  }
+
+  // Handle the `parserOptions` key
+  if ('parserOptions' in newConfig) {
+    newConfig.languageOptions.parserOptions = newConfig.parserOptions;
+    delete newConfig.parserOptions;
+  }
+
+  // Handle the `plugins` key
+  if ('plugins' in newConfig) {
+    newConfig.plugins = newConfig.plugins.reduce((acc, plugin) => {
+      if (plugin in handledPlugins) {
+        return { ...acc, [plugin]: handledPlugins[plugin] };
+      }
+      return acc;
+    }, {});
+  }
+
+  return newConfig;
+}
+
+module.exports = [
+  ...airbnbRules.map((rule) => convertIntoEslintFlatConfig(require(rule))),
+  convertIntoEslintFlatConfig(airbnbConfig)
+];
--- packages/eslint-config-airbnb-base/rules/imports.js
+++ packages/eslint-config-airbnb-base/rules/imports.js
@@ -91,7 +91,8 @@ module.exports = {
         '**/protractor.conf.js', // protractor config
         '**/protractor.conf.*.js', // protractor config
         '**/karma.conf.js', // karma config
-        '**/.eslintrc.js' // eslint config
+        '**/.eslintrc.js', // eslint config
+        '**/eslint.config.js', // eslint flat config
       ],
       optionalDependencies: false,
     }],
