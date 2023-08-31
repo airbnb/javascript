0113c0907ad7b0e7eea9f8eaba848c111686a177
Olivier Zalmanski <olivier.zalmanski@me.com>
2023-08-31 18:16:02 +0200

    Chnages after review

--- packages/eslint-config-airbnb-base/flat.js
+++ packages/eslint-config-airbnb-base/flat.js
@@ -9,14 +9,19 @@
 const {
   extends: airbnbRules,
   ...airbnbConfig
-} = require('eslint-config-airbnb-base');
+} = require('.');
 const importPlugin = require('eslint-plugin-import');
 const globals = require('globals');
 
-const mappedEnvs = {
+/**
+ * In the `globals` module, only the 'es6' key differs from the accepted ESLint `env` keys.
+ * The keys 'es2016', 'es2018', 'es2019', 'es2022', 'es2023' and 'es2024'
+ * are not handled by the `globals` module.
+ */
+const envMapping = {
   es6: 'es2015',
 };
-const handledPlugins = {
+const plugins = {
   import: importPlugin,
 };
 
@@ -25,17 +30,19 @@ function convertIntoEslintFlatConfig(config) {
 
   // Handle the `env` key
   if ('env' in newConfig) {
-    newConfig.languageOptions.globals = Object.keys(newConfig.env)
-      .filter((key) => newConfig.env[key] === true)
-      .reduce((acc, key) => {
-        if (key in globals) {
-          return { ...acc, ...globals[key] };
-        }
-        if (key in mappedEnvs && mappedEnvs[key] in globals) {
-          return { ...acc, ...globals[mappedEnvs[key]] };
-        }
-        return acc;
-      }, {});
+    newConfig.languageOptions.globals = Object.fromEntries(
+      Object.keys(newConfig.env)
+        .filter(
+          (key) => newConfig.env[key] === true &&
+          (key in globals ||
+            (key in envMapping && envMapping[key] in globals))
+        )
+        .flatMap((key) =>
+          (key in globals
+            ? Object.entries(globals[key])
+            : Object.entries(globals[envMapping[key]]))
+        )
+    );
     delete newConfig.env;
   }
 
@@ -47,12 +54,11 @@ function convertIntoEslintFlatConfig(config) {
 
   // Handle the `plugins` key
   if ('plugins' in newConfig) {
-    newConfig.plugins = newConfig.plugins.reduce((acc, plugin) => {
-      if (plugin in handledPlugins) {
-        return { ...acc, [plugin]: handledPlugins[plugin] };
-      }
-      return acc;
-    }, {});
+    newConfig.plugins = Object.fromEntries(
+      newConfig.plugins
+        .filter((plugin) => plugin in plugins)
+        .map((plugin) => [plugin, plugins[plugin]])
+    );
   }
 
   return newConfig;
@@ -60,5 +66,4 @@ function convertIntoEslintFlatConfig(config) {
 
 module.exports = [
   ...airbnbRules.map((rule) => convertIntoEslintFlatConfig(require(rule))),
-  convertIntoEslintFlatConfig(airbnbConfig)
-];
+  convertIntoEslintFlatConfig(airbnbConfig)];
