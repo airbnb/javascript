1c11c0d20c56e972df02729c233bfdfbcc91640d
Anurag chavan <118217089+anuragchvn-blip@users.noreply.github.com>
2025-03-05 21:33:47 +0530

    Update base.js
    Modify packages/eslint-config-airbnb/base.js to include the new rule.

--- packages/eslint-config-airbnb/base.js
+++ packages/eslint-config-airbnb/base.js
@@ -1,4 +1,7 @@
+const noConsoleInProd = require('./rules/no-console-in-prod');
+
 module.exports = {
-  extends: ['eslint-config-airbnb-base'].map(require.resolve),
-  rules: {},
+  rules: {
+    'no-console-in-prod': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
 };
